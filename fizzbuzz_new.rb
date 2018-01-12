6c1cf74872013ee592d9339b9541b7220f2f515c
triciahoover <tricia.l.hoover@gmail.com>
2018-01-12 13:24:26 -0500

    test works

--- fizzbuzz_new.rb
+++ fizzbuzz_new.rb
@@ -1,11 +1,13 @@
-require "minitest/autorun"
-require_relative "fizzbuzz_new.rb"  #You have to change this what is in quotes to the name of your rb files
+#Object of the program is to print 1 to 100 and if the the number is divisible by 3 then print "Fizz" and if the number is divisible by 5 then print "Buzz" and if the number is divisible by 3 and 5 then print "Fizzbuzz"
 
-class TestFizzbuzz < Minitest::Test  #The word Math is the name of your project
-
-	def test_assert_that_1_equals_1
-		assert_equal(1,1)
+def fizzbuzz(i)
+	if i % 15 == 0
+		i = "Fizzbuzz"
+	elsif i % 5 == 0
+		i = "Buzz"
+	elsif i % 3 == 0
+		i = "Fizz"
 	end
-
-	
+	i
 end
+
--- test.rb
+++ test.rb
@@ -7,5 +7,19 @@ class TestFizzbuzz < Minitest::Test  #The word Math is the name of your project
 		assert_equal(1,1)
 	end
 
+	def test_assert_that_1_equals_1
+		assert_equal(1,1)
+	end
+
+	def test_assert_that_3_returns_Fizz
+		assert_equal("Fizz", fizzbuzz(3))
+	end
 	
+	def test_assert_that_5_returns_Buzz
+		assert_equal("Buzz", fizzbuzz(5))
+	end
+	
+	def test_assert_that_15_returns_FizzBuzz
+		assert_equal("Fizzbuzz", fizzbuzz(15))
+	end
 end
